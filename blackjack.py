2b66a825639dc6e51f4a556733b5dbb61b9f6f05
Brian Ly <briancly@berkeley.edu>
2016-03-17 16:23:58 -0600

    Add dealer

--- blackjack.py
+++ blackjack.py
@@ -1,28 +1,45 @@
 import random
 
 total = 0
+dealer = 0
 
 def draw():
 	hand = random.randint(1,11)
 	return hand
 
 for _ in range(2):
-	card = draw()
-	total += card
-	print 'Card: ' + str(card)
+	user_card = draw()
+	dealer_card = draw()
+	total += user_card
+	dealer += dealer_card
+	print 'Card: ' + str(user_card)
 
 print 'Total: ' + str(total)
 
 while total < 21:
 	hit = raw_input('Hit? (Y/N): ')
 	if hit == 'y':
-		card = draw()
-		total += card
-		print 'Card: ' + str(card)
+		user_card = draw()
+		total += user_card
+		print 'Card: ' + str(user_card)
 		print 'Total: ' + str(total)
 	elif hit == 'n':
+		print 'Dealer Total: ' + str(dealer)
+		while dealer < 15:
+			dealer_card = draw()
+			dealer += dealer_card
+			print 'Dealer Card: ' + str(dealer_card)
+			print 'Dealer Total: ' + str(dealer)
+			if dealer > 21:
+				print 'Dealer a busta. You win!'
+		if total < dealer:
+			print 'Dealer wins!'
+		elif total == dealer:
+			print 'Tie game!'
+		else:
+			print 'You win!'
 		break
 	else:
-		print 'What is that?'
+		print 'I do not recognize your response.'
 else:
 	print 'You a busta'
